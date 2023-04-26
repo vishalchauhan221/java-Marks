da2d8811cb87a35f6835853528892e75de91cc49
vishalchauhan221 <vishalkmr1277>
2023-04-26 11:54:51 +0530

    first commit

---
+++ vw.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Taking the input marks from  the examiner");
+System.out.println("all the subject marks in six semester exam");
+
+System.out.println("enter the first sub math");
+int math=sc.nextInt();
+System.out.println("enter the second sub mc");
+int mc=sc.nextInt();
+System.out.println("enter the third sub tt");
+int tt=sc.nextInt();
+System.out.println("enter the fourth sub ai");
+int ai=sc.nextInt();
+System.out.println("enter the fifth sub ip");
+int ip=sc.nextInt();
+float percentage=((math + mc + tt + ai + ip)/500.0f)*100;
+System.out.println("total percentage of semester exam");
+System.out.println(percentage);
+}
+}
